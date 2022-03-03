f80ca09904129ab010d1b6be21b63c6fbb855219
Jakhongir <anorboev.jahongir8007@gmail.com>
2022-03-03 16:54:00 +0500

    GetTotals method added

--- migrations/000003_create_wallets_income_and_expense_table.up.sql
+++ migrations/000003_create_wallets_income_and_expense_table.up.sql
@@ -1,5 +1,13 @@
-CREATE TABLE IF NOT EXISTS wallets_income (
+CREATE TABLE IF NOT EXISTS wallets_incomes (
     income_id UUID PRIMARY KEY,
     wallet_id UUID REFERENCES wallets(wallet_id),
+    amount DECIMAL(15,4) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS wallets_expenses (
+    income_id UUID PRIMARY KEY,
+    wallet_id UUID REFERENCES wallets(wallet_id),
+    amount DECIMAL(15,4) NOT NULL,
     created_at TIMESTAMP NOT NULL,
 );
--- storage/postgres/postgres.go
+++ storage/postgres/postgres.go
@@ -15,7 +15,7 @@ func NewDatabase(db *sqlx.DB) *Database {
 		db: db,
 	}
 }
-
+// This method returs balance of the wallet
 func (d Database) GetBalance(w models.Wallet) (*models.Wallet, error) {
 
 	query := `select balance where wallet_id = $1 and delted_at is null`
@@ -26,7 +26,7 @@ func (d Database) GetBalance(w models.Wallet) (*models.Wallet, error) {
 	}
 	return &models.Wallet{w.Id, w.Balance}, nil
 }
-
+// This method checks wheather wallet exists or not, if not it returns error and Wallet with null fields
 func (d Database) CheckWalletExists(w models.Wallet) (*models.Wallet, error) {
 	var exists int
 	query := `select count(*) where wallet_id = $1 and delted_at is null`
@@ -41,3 +41,28 @@ func (d Database) CheckWalletExists(w models.Wallet) (*models.Wallet, error) {
 
 	return d.GetBalance(w)
 }
+
+// This method returns total operations and total expense and total income for current month
+func (d Database) GetTotals(w models.Wallet) (*models.WalletHistory,error) {
+	var walletHistory = models.WalletHistory{}
+	query := `SELECT balance, SUM(wi.amount),SUM(we.amount),COUNT(wi.amount),COUNT(we.amount) FROM wallets w JOIN wallets_income wi USING(wallet_id) JOIN wallets_expenses we USING(wallet_id) GROUP BY wallet_id having wallet_id = $1`
+
+	err := d.db.DB.QueryRow(query,w.Id).Scan(
+		&walletHistory.CurrentBalance,
+		&walletHistory.TotalIncome,
+		&walletHistory.TotalExpense,
+		&walletHistory.TotalIncomeOperations,
+		&walletHistory.TotalExpenseOperations,
+	)
+
+	if err != nil {
+		return nil,err
+	}
+
+	walletHistory.Id = w.Id
+	walletHistory.TotalOperations = walletHistory.TotalExpenseOperations + walletHistory.TotalIncomeOperations
+
+	return &walletHistory,nil
+
+}
+
