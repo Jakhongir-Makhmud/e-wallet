a58ca5a2922a0b804eadbdd5fa0a2c1c90b3cd71
Jakhongir <anorboev.jahongir8007@gmail.com>
2022-03-04 12:32:18 +0500

    GetBalance handler added

--- api/handlers/GetBalance.go
+++ api/handlers/GetBalance.go
@@ -1,9 +1,53 @@
 package handlers
 
-import "github.com/gin-gonic/gin"
+import (
+	"e-wallet/storage/models"
+	"encoding/json"
+	"net/http"
 
-
+	"github.com/gin-gonic/gin"
+)
 
 func (h handlers) GetBalance(c *gin.Context) {
-	
+
+	bodyRequest := models.Wallet{}
+
+	err := c.ShouldBindJSON(&bodyRequest)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "error while binding json",
+		})
+		return
+	}
+
+	w,err := h.repo.CheckWalletExists(bodyRequest)
+
+	if w.Id == "" || err != nil {
+		c.JSON(http.StatusBadRequest,gin.H{
+			"error":"no such wallet",
+		})
+		return
+	}
+
+
+	balance, err := h.repo.GetBalance(bodyRequest)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"err": "something went wrong",
+		})
+		return
+	}
+
+	balanceByte, err := json.Marshal(balance)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": "something went wrong, please try agan",
+		})
+		return
+	}
+	bodyHex := h.auth.HashBody(balanceByte)
+	c.Writer.Header().Set("X-Digest", bodyHex)
+
+	c.JSON(http.StatusOK,balance)
 }
